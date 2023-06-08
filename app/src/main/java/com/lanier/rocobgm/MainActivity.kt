2c6ec8f4bd59ee19c451c26101c8db22df42af9a
幻弦让叶 <1248389474@qq.com>
2023-06-08 21:22:33 +0800

    主页面完善

--- app/src/main/java/com/lanier/rocobgm/MainActivity.kt
+++ app/src/main/java/com/lanier/rocobgm/MainActivity.kt
@@ -2,11 +2,23 @@ package com.lanier.rocobgm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.commit
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.imageview.ShapeableImageView
+import com.lanier.rocobgm.compose.ComposeDuration
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,6 +26,15 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var environment: IPlayEvent
+    private val vm by viewModels<MainVM>(factoryProducer = {
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return MainVM(environment as SongEnvironment) as T
+            }
+        }
+    })
+
+    private val percentFlow = MutableStateFlow(0)
 
     private val ivAvatar by lazy {
         findViewById<ShapeableImageView>(R.id.ivAvatar)
@@ -21,28 +42,93 @@ class MainActivity : AppCompatActivity() {
     private val tvTitle by lazy {
         findViewById<TextView>(R.id.tvTitle)
     }
+    private val progressBar by lazy {
+        findViewById<ProgressBar>(R.id.progressBar)
+    }
     private val ivController by lazy {
         findViewById<ImageView>(R.id.ivController)
     }
+    private val composeDuration by lazy {
+        findViewById<ComposeView>(R.id.composeDurationBar)
+    }
+    private val toolbar by lazy {
+        findViewById<MaterialToolbar>(R.id.toolbar)
+    }
+
+    private var isPlaying = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        environment.init(this)
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.frameLayout, HomeFra(), "home")
-            .commit()
+        setSupportActionBar(toolbar)
 
         ivController.setOnClickListener {
+            if (isPlaying) {
+                playActionFlow.tryEmit(PlayAction.Pause)
+            } else {
+                playActionFlow.tryEmit(PlayAction.Resume)
+            }
         }
+        composeDuration.setContent {
+            ComposeDuration(percentFlow)
+        }
+
+        environment.init(this)
+        vm.lazyInit()
+
+        supportFragmentManager
+            .commit {
+                add(R.id.frameLayout, HomeFra(), "home")
+            }
 
         lifecycleScope.launch {
             playStateFlow.collect {
-                tvTitle.text = it.curPlaySong.sceneName
+                when (it) {
+                    PlayDataState.Idle -> {}
+                    is PlayDataState.LoadingState -> {
+                        if (it.loading) {
+                            progressBar.visible()
+                            ivController.disabled()
+                        } else {
+                            progressBar.invisible()
+                            ivController.enabled()
+                        }
+                    }
+                    is PlayDataState.PlayData -> {
+                        tvTitle.text = it.data.sceneName
+                        vm.play(it.data)
+                    }
+                    is PlayDataState.PlayDuration -> {
+                        percentFlow.tryEmit(it.percent)
+                    }
+                    is PlayDataState.PlayState -> {
+                        isPlaying = it.playing
+                        if (it.playing) {
+                            ivController.setImageResource(R.drawable.baseline_pause_24)
+                        } else {
+                            ivController.setImageResource(R.drawable.baseline_play_arrow_24)
+                        }
+                    }
+                }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_sync) {
+            vm.obtainData(assets, true)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        playActionFlow.tryEmit(PlayAction.Release)
     }
 }
